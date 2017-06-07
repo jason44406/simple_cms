d15a943c272cd7b775780f33b8079d934c234a68
Jason Cachia <jcachia@gannett.com>
2017-06-07 16:01:24 -0400

    Chap 9 Complete

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,7 @@
 class SubjectsController < ApplicationController
 
+  layout 'admin'
+
   def index
     @subjects = Subject.sorted
   end
@@ -19,14 +21,14 @@ class SubjectsController < ApplicationController
     if @subject.save
       # If save succeeds, redirect to the index action.
       # save always returns true or false.
-      flash[:notice] = "Subject created successfully!"
+      flash[:notice] = "Subject '#{@subject.name}' created successfully!"
       redirect_to(subjects_path)
     else
       # If the save fails, redisplay the form so the user can correct
       # Render just renders the template with fields populated.
+      flash[:error] = "Subject not created!"
       render('new')
     end
-
   end
 
   def edit
@@ -40,14 +42,14 @@ class SubjectsController < ApplicationController
     if @subject.update_attributes(subject_params)
       # If save succeeds, redirect to the show action.
       # save always returns true or false.
-      flash[:notice] = "Subject updated successfully!"
+      flash[:notice] = "Subject '#{@subject.name}' updated successfully!"
       redirect_to(subject_path(@subject))
     else
       # If the save fails, redisplay the form so the user can correct
       # Render just renders the template with fields populated.
+      flash[:error] = "Subject not saved!"
       render('edit')
     end
-
   end
 
   def delete
@@ -56,9 +58,13 @@ class SubjectsController < ApplicationController
 
   def destroy
     @subject = Subject.find(params[:id])
-    @subject.destroy
-    flash[:notice] = "Subject '#{@subject.name}' destroyed successfully!"
-    redirect_to(subjects_path)
+    if @subject.destroy
+      flash[:notice] = "Subject '#{@subject.name}' destroyed successfully!"
+      redirect_to(subjects_path)
+    else
+      flash[:error] = "Subject not deleted!"
+      render('delete')
+    end
   end
 
   private
